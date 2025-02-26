6962e4974b3a9c97296f870b939988ad68103427
Elias Siqueira <eliaslsiqueira@gmail.com>
2025-02-26 14:17:11 -0400

    Update Logo.tsx

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -6,7 +6,7 @@ export function Logo(props: LogoProps) {
   return (
     <svg
       width={106}
-      height={94}
+      height={95}
       viewBox="0 0 106 94"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
